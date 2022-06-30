de13a963a40bf9e9c3d180e187fb67590dc42ae1
Alexej Grigorjew <alex.grigorjew@stud-mail.uni-wuerzburg.de>
2022-06-30 14:10:13 +0200

    pyskript aktualisiert

--- py_hwtstamp_sniffer/pydump.py
+++ py_hwtstamp_sniffer/pydump.py
@@ -16,10 +16,13 @@ SIOCGIFFLAGS = 0x8913
 SIOCSIFFLAGS = 0x8914
 SIOCSHWTSTAMP = 0x89b0
 SO_TIMESTAMPNS = 35  # or 64 ... idk
+SO_TIMESTAMPING = 37  # or 65?
 PACKET_TIMESTAMP = 17
 SOL_PACKET = 263
 PACKET_RX_RING = 5
+SOF_TIMESTAMPING_RX_HARDWARE = (1<<2)
 SOF_TIMESTAMPING_RAW_HARDWARE = (1<<6)
+HWTSTAMP_TX_OFF = 0
 HWTSTAMP_TX_ON = 1  # /usr/include/linux/net_tstamp.h:111
 HWTSTAMP_FILTER_ALL = 1  # /usr/include/linux/net_tstamp.h:140
 
@@ -91,39 +94,43 @@ def main(args):
         fcntl.ioctl(s.fileno(), SIOCSIFFLAGS, ifr)
 
         # request nanosecond resolution
-        s.setsockopt(socket.SOL_SOCKET, SO_TIMESTAMPNS, 1)
+        s.setsockopt(socket.SOL_SOCKET, SO_TIMESTAMPING, SOF_TIMESTAMPING_RX_HARDWARE | SOF_TIMESTAMPING_RAW_HARDWARE)
 
         # request hardware timestamps
-        conf = HWTSTAMP_CONFIG(0, HWTSTAMP_TX_ON, HWTSTAMP_FILTER_ALL)
+        conf = HWTSTAMP_CONFIG(0, HWTSTAMP_TX_OFF, HWTSTAMP_FILTER_ALL)
         ifr = HWTSTAMP_IFREQ(args.int_b, pointer(conf))
         if x := fcntl.ioctl(s.fileno(), SIOCSHWTSTAMP, ifr) != 0:
             raise ValueError(f"fcntl.ioctl(SIOCSHWTSTAMP) returned {x}")
 
         # hardware timestamp socket options
-        s.setsockopt(SOL_PACKET, PACKET_TIMESTAMP, SOF_TIMESTAMPING_RAW_HARDWARE)
-        rxring_conf = struct.pack("IIII", 4096*4096, 1, 4096, 4096)
-        s.setsockopt(SOL_PACKET, PACKET_RX_RING, rxring_conf)
+        #s.setsockopt(SOL_PACKET, PACKET_TIMESTAMP, SOF_TIMESTAMPING_RAW_HARDWARE)
+        #rxring_conf = struct.pack("IIII", 4096*4096, 1, 4096, 4096)
+        #s.setsockopt(SOL_PACKET, PACKET_RX_RING, rxring_conf)
 
         while True:
             # read 1 packet
             raw_data, ancdata, flags, address = s.recvmsg(65535, 1024)
 
+            #print("---")
             #for cmsg_level, cmsg_type, cmsg_data in ancdata:
-            #    if cmsg_level == socket.SOL_SOCKET and cmsg_type == SO_TIMESTAMPNS:
-            #        print(f"   {cmsg_level=}, {cmsg_type=}, {cmsg_data=}")
+                #print(f"   {cmsg_level=}, {cmsg_type=}, {cmsg_data=}")
+                #if cmsg_level == socket.SOL_SOCKET and cmsg_type == SO_TIMESTAMPNS:
+                #    print(f"   {cmsg_level=}, {cmsg_type=}, {cmsg_data=}")
 
             # unpack auxiliary stuff
-            ts_sec, ts_nsec = struct.unpack("@QQ", ancdata[0][2])  # 2x unsigned long long
-            ts_human = datetime.fromtimestamp(ts_sec).strftime("%H:%M:%S") + "." + str(ts_nsec)  # %Y-%m-%d
+            #print(ancdata[0][2])
+            if len(ancdata) > 0:
+                _, _, _, _, ts_sec, ts_nsec = struct.unpack("@QQQQQQ", ancdata[0][2])  # 2x unsigned long long
+                ts_human = datetime.fromtimestamp(ts_sec).strftime("%H:%M:%S") + "." + str(ts_nsec)  # %Y-%m-%d
 
-            # unpack headers
-            eth_hdr = struct.unpack("!6s6s2s", raw_data[0:14])  # 6 dst MAC, 6 src MAC, 2 ethType
-            #ipHeader = raw_data[14:34]
-            #ip_hdr = struct.unpack("!12s4s4s", ipHeader)  # 12s represents Identification, Time to Live, Protocol | Flags, Fragment Offset, Header Checksum
-            #tcpHeader = raw_data[34:54]
-            #tcp_hdr = struct.unpack("!HH16s", tcpHeader)
+                # unpack headers
+                eth_hdr = struct.unpack("!6s6s2s", raw_data[0:14])  # 6 dst MAC, 6 src MAC, 2 ethType
+                #ipHeader = raw_data[14:34]
+                #ip_hdr = struct.unpack("!12s4s4s", ipHeader)  # 12s represents Identification, Time to Live, Protocol | Flags, Fragment Offset, Header Checksum
+                #tcpHeader = raw_data[34:54]
+                #tcp_hdr = struct.unpack("!HH16s", tcpHeader)
 
-            print(f"{ts_human} \t {_mac(eth_hdr[1])} -> {_mac(eth_hdr[0])}, type={_hex(eth_hdr[2])}, hash={raw_data[14:]}", flush=True)
+                print(f"{ts_human} \t {_mac(eth_hdr[1])} -> {_mac(eth_hdr[0])}, type={_hex(eth_hdr[2])}, hash={raw_data[14:]}", flush=True)
 
     except KeyboardInterrupt:
         print("Shutdown requested... exiting")
